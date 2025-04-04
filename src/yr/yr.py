4ae445a36108caa97c85411e092735cb26c760eb
axelbech <47354172+axelbech@users.noreply.github.com>
2025-04-04 15:43:04 +0200

    Add User-Agent headers according to YR TOS

--- src/yr/yr.py
+++ src/yr/yr.py
@@ -5,12 +5,13 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("YR")
 
 YR_API_BASE = "https://api.met.no/weatherapi/locationforecast/2.0"
+USER_AGENT = "https://github.com/AnyContext-ai/mcp-servers/blob/main/src/yr/yr.py"
 
 async def make_weather_forecast_request(type: Literal["compact", "complete"], latitude: float, longitude: float, altitude: Optional[int] = None) -> Any:
     """Execute the weather forecast query
 
     Args:
-        type (Literal[&quot;compact&quot;, &quot;complete&quot;]): "compact or complete weather forecast"
+        type (Literal["compact", "complete"]): "compact or complete weather forecast"
         latitude (float): Latitude of the location
         longitude (float): Longitude of the location
         altitude (Optional[int], optional): Optional altitude of the location. Defaults to None.
@@ -19,9 +20,14 @@ async def make_weather_forecast_request(type: Literal["compact", "complete"], la
         Any: JSON response
     """
     url = f"{YR_API_BASE}/{type}?lat={latitude}&lon={longitude}"
-    if altitude != None:
-        url = f"{url}&altitude={altitude}"
-    async with httpx.AsyncClient() as client:
+    if altitude is not None:
+        url += f"&altitude={altitude}"
+
+    headers = {
+        "User-Agent": USER_AGENT
+    }
+
+    async with httpx.AsyncClient(headers=headers) as client:
         try:
             response = await client.get(url)
             response.raise_for_status()
@@ -40,11 +46,12 @@ async def get_weather_forecast_compact(latitude: float, longitude: float, altitu
     """
     result = await make_weather_forecast_request(
         type='compact',
-        latitude=latitude, 
-        longitude=longitude
+        latitude=latitude,
+        longitude=longitude,
+        altitude=altitude
     )
     return result
-        
+
 @mcp.tool()
 async def get_weather_forecast_complete(latitude: float, longitude: float, altitude: Optional[int] = None):
     """Get a complete weather forecast for a specified location
@@ -56,14 +63,11 @@ async def get_weather_forecast_complete(latitude: float, longitude: float, altit
     """
     result = await make_weather_forecast_request(
         type='complete',
-        latitude=latitude, 
-        longitude=longitude
+        latitude=latitude,
+        longitude=longitude,
+        altitude=altitude
     )
     return result
 
 if __name__ == "__main__":
     mcp.run(transport="sse")
-        
-    
-            
-            
